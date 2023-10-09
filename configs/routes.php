0fdc7d36761525673c40b45ed1e14cc3094cf1ba
khoa.pa <bob@gmail.com>
2023-10-09 20:53:39 +0700

    update router

--- configs/routes.php
+++ configs/routes.php
@@ -1,4 +1,4 @@
 <?php 
-$routes['default_controller'] = 'home';
+$routes['default_controller'] = 'home'; 
 $routes['test'] = 'home/index';
 $routes['test1/.+-(\d+).html'] = 'home/detail/$1'; ///test1/bai-1.html
